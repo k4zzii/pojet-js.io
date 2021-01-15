27b4a90f3bff23ad88f7847937661eee31a0f6d1
k4zzi <pauldesbiendas@gmail.com>
2021-01-15 12:15:03 +0100

    done

---
+++ assets/js/script.js
@@ -0,0 +1,228 @@
+function showhome(){
+    $('.home').show();
+    $('.contact').hide();
+    $('.contactlink').show();
+    $('.homelink').hide();
+    $('.about').hide();
+    $('.top').show();
+    $('.footer').addClass('footer');
+    $('.footer').removeClass('footer2');
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showcontact(){
+    $('.contact').show();
+    $('.home').hide();
+    $('.about').hide();
+    $('.contactlink').hide();
+    $('.homelink').show()
+    $('.footer').addClass('footer2');
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showabout(){
+    $('.about').show();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').hide();
+    $('.footer').addClass('footer2');
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker1(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker1').show();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker2(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker2').show();
+    $('.sneaker1').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker3(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker3').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker4(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker4').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker5(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker5').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker6(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker6').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker7').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker7(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker7').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker8').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker8(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker8').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker7').hide();
+    $('.sneaker9').hide();
+}
+
+function showsneaker9(){
+    $('.about').hide();
+    $('.contact').hide();
+    $('.home').hide();
+    $('.contactlink').show();
+    $('.homelink').show()
+    $('.top').show();
+    $('.footer').addClass('footer2');
+    $('.sneaker9').show();
+    $('.sneaker1').hide();
+    $('.sneaker2').hide();
+    $('.sneaker3').hide();
+    $('.sneaker4').hide();
+    $('.sneaker5').hide();
+    $('.sneaker6').hide();
+    $('.sneaker8').hide();
+    $('.sneaker7').hide();
+}
+showhome()
